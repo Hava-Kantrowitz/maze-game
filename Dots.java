b91000fd38c8296dea1e9418b2e9ed9dc341c772
Hava-Kantrowitz <46357722+Hava-Kantrowitz@users.noreply.github.com>
2019-06-29 14:24:47 -0400

    Basic setup
    Generates the maze and allows you to move player

---
+++ Dots.java
@@ -0,0 +1,123 @@
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.event.KeyEvent;
+import java.awt.geom.Ellipse2D;
+
+import javax.swing.JPanel;
+
+public class Dots extends JPanel{
+	
+	protected int x;
+	protected int y;
+	protected int width;
+	protected int height;
+	protected Color color;
+	protected boolean visible;
+	protected Graphics2D g2d;
+	private int dx;
+	private int dy;
+	
+	public Dots(Color color, int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.color = color;
+		visible = true;
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		drawDot(g);
+	}
+	
+	public void drawDot(Graphics g) {
+		Ellipse2D e = new Ellipse2D.Double(x, y, width, height);//create a 2d ellipse at the given coordinates
+		Graphics2D g2d = (Graphics2D) g;//cast the graphics into 2d graphics
+		
+		RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);//RH makes graphics
+		//look better by enabling hints, turning antialias on makes things look more lifelike but code slower, antialias off is
+		//less lifelike but faster performance
+		
+		rh.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+		g2d.setRenderingHints(rh);//sets the 2d graphics to the rendering style created
+		g2d.setStroke(new BasicStroke(5));//Stroking a Shape is like tracing its outline with a marking pen of the appropriate size and shape. 
+		//The area where the pen would place ink is the area enclosed by the outline Shape.
+		g2d.setColor(color);
+		g2d.draw(e);
+		
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public boolean isVisible() {
+		return visible;
+	}
+	
+	public void setVisible(Boolean visible) {
+		this.visible = visible;
+	}
+	
+	//rectangle bounds needed for collision detection!
+	public Rectangle getBounds() {
+		return new Rectangle(x, y, width, height);
+	}
+	
+	public void move() {
+		x += dx;
+		y += dy;
+	}
+	
+	//key pressed and key released controlled with key event
+		public void keyPressed(KeyEvent e) {
+			int key = e.getKeyCode();
+			
+			if(key == KeyEvent.VK_LEFT) {
+				dx = -1;
+			}
+			
+			if(key == KeyEvent.VK_RIGHT) {
+				dx = 1;
+			}
+			
+			if(key == KeyEvent.VK_UP) {
+				dy = -1;
+			}
+			
+			if(key == KeyEvent.VK_DOWN) {
+				dy = 1;
+			}
+		}
+		
+		public void keyReleased(KeyEvent e) {
+			int key = e.getKeyCode();
+			
+			if(key == KeyEvent.VK_LEFT) {
+				dx = 0;
+			}
+			
+			if(key == KeyEvent.VK_RIGHT) {
+				dx = 0;
+			}
+			
+			if(key == KeyEvent.VK_UP) {
+				dy = 0;
+			}
+			
+			if(key == KeyEvent.VK_DOWN) {
+				dy = 0;
+			}
+		}
+
+}
---
+++ MazeMaker.java
@@ -0,0 +1,190 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.Path2D;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.Timer;
+ 
+public class MazeMaker extends JPanel implements ActionListener {
+    enum Dir {
+        N(1, 0, -1), S(2, 0, 1), E(4, 1, 0), W(8, -1, 0);//the directions
+        final int bit;//given cell
+        final int dx;//the movement in the x direction
+        final int dy;//the movement in the y directions
+        Dir opposite;//the direction opposite the given direction
+ 
+        // initializes all opposite values
+        static {
+            N.opposite = S;
+            S.opposite = N;
+            E.opposite = W;
+            W.opposite = E;
+        }
+ 
+        Dir(int bit, int dx, int dy) {//the direction enum
+            this.bit = bit;//the given cell
+            this.dx = dx;//the movement in x
+            this.dy = dy;//the movement in y
+        }
+    };
+    final int nCols;//number of columns in maze
+    final int nRows;//number of rows in maze
+    final int cellSize = 25;//pixel size of each cells
+    final int margin = 25;//pixel size of the margin
+    final int[][] maze;//the maze
+    final int DELAY = 10;//the repaint speed
+    private Timer timer;//the timer
+    Dots playerDot;//the player piece
+    Dots startDot;//the start location
+    Dots finalDot;//the end location
+ 
+    /**
+     * Creates the maze generator and maze solver
+     * @param size the size of the board, how many rows/columns it would be
+     */
+    public MazeMaker(int size) {
+        setPreferredSize(new Dimension(650, 650));//set the size
+        setBackground(Color.white);//set the background color
+        nCols = size;//set the number of rows and cols to the given size
+        nRows = size;//making it a square
+        maze = new int[nRows][nCols];//initialize the maze with the correct number of rows and columns
+        generateMaze(0, 0);//generate the maze starting at coordinate (0,0)
+        initDots();
+        setFocusable(true);
+ 
+        addKeyListener(new TAdapter());
+        timer = new Timer(DELAY, this);
+		timer.start();
+    }
+    
+    public void initDots() {
+    	int offset = margin + cellSize / 2;//create the animation offset
+        
+        startDot = new Dots(Color.blue, offset-5, offset-5, 10, 10);
+        playerDot = new Dots(Color.pink, offset-5, offset-5, 10, 10);
+        int x = offset + (nCols - 1) * cellSize;//set the x value
+        int y = offset + (nRows - 1) * cellSize;//set the y value
+        finalDot = new Dots(Color.green, x-5, y-5, 10, 10);
+    }
+ 
+    @Override
+    /**
+     * Paints the maze and the solution
+     * @param g the graphics input
+     */
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);//super it up to the component
+        drawMaze(g);
+        drawDots(g);
+    }
+    
+    public void drawMaze(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;//make it a 2d drawing
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);//turn on rendering hints
+ 
+        g2d.setStroke(new BasicStroke(5));//set the size of the stroke
+        g2d.setColor(Color.black);//set the color to black
+ 
+        // draw maze
+        for (int r = 0; r < nRows; r++) {//for each row and columns
+            for (int c = 0; c < nCols; c++) {
+ 
+                int x = margin + c * cellSize;//the x is the column number adjusted for cell size and margin
+                int y = margin + r * cellSize;//the y is the row number adjusted for cell size and margin
+ 
+                if ((maze[r][c] & 1) == 0) // Draws north
+                    g2d.drawLine(x, y, x + cellSize, y);
+ 
+                if ((maze[r][c] & 2) == 0) // Draws south
+                    g2d.drawLine(x, y + cellSize, x + cellSize, y + cellSize);
+ 
+                if ((maze[r][c] & 4) == 0) // Draws east
+                    g2d.drawLine(x + cellSize, y, x + cellSize, y + cellSize);
+ 
+                if ((maze[r][c] & 8) == 0) // Draws west
+                    g2d.drawLine(x, y, x, y + cellSize);
+            }
+        }
+        
+ 
+    }
+    
+    public void drawDots(Graphics g) {
+    	startDot.drawDot(g);
+    	playerDot.drawDot(g);
+    	finalDot.drawDot(g);
+    	
+    }
+ 
+    /**
+     * Generates a maze using the recursive backtracking algorithm
+     * @param r the row the current cell is in
+     * @param c the column the current cell is in
+     */
+    void generateMaze(int r, int c) {
+        Dir[] dirs = Dir.values();//create a new list of directions
+        Collections.shuffle(Arrays.asList(dirs));//shuffle the list of directions
+        for (Dir dir : dirs) {//for each direction in the directions list
+            int nc = c + dir.dx;//adjust the new column
+            int nr = r + dir.dy;//adjust the new row
+            if (withinBounds(nr, nc) && maze[nr][nc] == 0) {//if it is within the bounds and not equal to 0
+                maze[r][c] |= dir.bit;//set the row and column
+                maze[nr][nc] |= dir.opposite.bit;//set the new row and column
+                generateMaze(nr, nc);//recursively generate the maze 
+            }
+        }
+    }
+ 
+    /**
+     * Determines if the cell at the given row and column is within the bounds of the board
+     * @param r the row of the current cell
+     * @param c the column of the current cell
+     * @return true if within bounds, false otherwise
+     */
+    boolean withinBounds(int r, int c) {
+        return c >= 0 && c < nCols && r >= 0 && r < nRows;//returns true if it is within the rounds of the board, false otherwise
+    }
+    
+    public void updateDots() {
+    	playerDot.move();
+    }
+ 
+ 
+    /**
+     * Main function to set up the board and maze
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            JFrame f = new JFrame();//create the new frame
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//set the close operation
+            f.setTitle("Maze Game");//set the title
+            f.setResizable(false);//not resizable
+            f.add(new MazeMaker(24), BorderLayout.CENTER);//add the maze to the board
+            f.pack();//pack size
+            f.setLocationRelativeTo(null);//set location
+            f.setVisible(true);//set the maze to visible
+        });
+    }
+    
+  //listen for key events, which are delegated to ship class
+  	private class TAdapter extends KeyAdapter{
+  		@Override
+  		public void keyReleased(KeyEvent e) {
+  			playerDot.keyReleased(e);
+  		}
+  		
+  		@Override
+  		public void keyPressed(KeyEvent e) {
+  			playerDot.keyPressed(e);
+  		}
+  	}
+
+@Override
+public void actionPerformed(ActionEvent arg0) {
+	updateDots();
+	repaint();
+	
+}
+
+}
